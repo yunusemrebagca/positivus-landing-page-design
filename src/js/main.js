030ea22a554f3f8f13649711d45f6af0a664a47a
yunusemrebagca <a7yunusemre34@gmail.com>
2023-05-08 02:05:10 +0300

    feat: added accordion section

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,30 @@ const toggleElement = () => {
 
 closeButton.addEventListener("click", () => toggleElement());
 openButton.addEventListener("click", () => toggleElement());
+
+const accordionCards = document.querySelectorAll(".accordion__card");
+
+accordionCards.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.querySelector(".accordion__card-description").classList.toggle("open");
+
+    document
+      .querySelectorAll(".accordion__card-description")
+      .forEach((item1, index1) => {
+        if (index != index1) {
+          item1.classList.remove("open");
+        }
+      });
+    accordionCards.forEach((item1) => {
+      if (
+        item1
+          .querySelector(".accordion__card-description")
+          .classList.contains("open")
+      ) {
+        item1.querySelector(".icon i").classList.replace("fa-plus", "fa-minus");
+      } else {
+        item1.querySelector(".icon i").classList.replace("fa-minus", "fa-plus");
+      }
+    });
+  });
+});
